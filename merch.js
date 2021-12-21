57e6299a6d82dbf052ff6802303f09495a773390
Philip Pounds <56984782+philip-hub@users.noreply.github.com>
2021-12-21 23:12:53 +0000

    a lot of commits

--- merch.js
+++
@@ -1,6 +0,0 @@
-// $('#changeImage').change(function(){
-  
-//  window.alert("chips") 
-//  $('#image')[0].src = this.value;
-// });
-// why was that here 👀
--- script.js
+++ script.js
@@ -1,51 +1,7 @@
 function nextTime() {
 
-  var currentDate = new Date();
-  var hours = currentDate.getHours();
-  var currentDay = currentDate.getDay();
-  var mm = currentDate.getMonth() + 1;
-  var dd = currentDate.getDate();
-  var yyyy = currentDate.getFullYear();
-  currentDate = mm + '/' + dd + '/' + yyyy;
 
-  currentDate = new Date(currentDate);
-
-  var referenceDate = new Date("06/25/2021");
-  var days = currentDate.getTime() - referenceDate.getTime();
-  days = days / (1000 * 3600 * 24);
-
-  var weeks = (days / 7);
-
-  //console.log(currentDay);
-
-  if (currentDay == 5) {
-    if (hours >= 20) {
-      weeks = Math.trunc(weeks) + 1;
-    } else {
-      weeks = Math.trunc(weeks);
-    }
-  } else {
-    weeks = Math.trunc(weeks) + 1;
-  }
-  var i;
-  var add;
-
-  //console.log(weeks);
-
-
-  var nextMeeting = referenceDate;
-
-
-  for (i = weeks; i < weeks * 2; i++) {
-    nextMeeting.setDate(nextMeeting.getDate() + 7);
-  }
-
-  var mm = nextMeeting.getMonth() + 1;
-  var dd = nextMeeting.getDate();
-  var yyyy = nextMeeting.getFullYear();
-  nextMeeting = mm + '/' + dd + '/' + yyyy;
-
-  var text = "Zoom 4 PM Friday " + nextMeeting;
+  var text = "Running June 10th to 12th!";
 
   return text;
 
