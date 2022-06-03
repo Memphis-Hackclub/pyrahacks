ca88365ac379ca15594085719add019103197d7e
Philip Pounds <56984782+philip-hub@users.noreply.github.com>
2022-06-03 15:48:55 -0500

    added Give My Certificate

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -81,12 +81,18 @@ export default {
           url: "https://www.leading-learners.com",
           image: "Leading Learners.png",
         },
+          {
+          name: "Give My Certificate",
+          url: "https://givemycertificate.com",
+          image: "GMC LogoS.png",
+        },
         {
           name: "Sticker Mule",
           url: "https://stickermule.com/",
           image: "stickermule-logo.svg",
         },
-           
+        
+          
       ],
     };
   },
